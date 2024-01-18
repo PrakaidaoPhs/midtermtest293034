53bd71b9487a1274082b6c3fe61c3b44e243efa6
nannaryy <117610863+nannary@users.noreply.github.com>
2024-01-18 10:13:01 +0700

    nan view.py

--- travelLog_app/views.py
+++ travelLog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 # def home(request):
@@ -9,3 +9,17 @@ def list(request):
     show_emp = Place.objects.all()
     context = {'Placedata':show_emp}
     return render(request,'list.html',context)
+
+def add(request): #เพิ่มข้อมูล
+    return render(request,'add.html') #ชื่อไฟล์ที่จะแสดง
+
+def edit(request): #จัดการข้อมูล
+    return render(request,'edit.html') #ชื่อไฟล์ที่จะแสดง
+
+def manage(request): #จัดการข้อมูล
+    return render(request,'show.html') #ชื่อไฟล์ที่จะแสดง
+
+def delete_place(request,pk):
+    return redirect('/manage_drug')
+
+
